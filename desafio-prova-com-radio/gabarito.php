78ab6ed4af24697266a1fc58630d6ee6db01a4bc
O Nacélio <nd7058898@gmail.com>
2022-10-23 09:51:39 -0300

    estruttura-de-verificação-melhorada

--- desafio-prova-com-radio/gabarito.php
+++ desafio-prova-com-radio/gabarito.php
@@ -16,50 +16,28 @@
     <main>
         <div class="gabaritos">
         <?php
-        $gabaritoOficial = ['D', 'D', 'B', 'C', 'A'];
-        $gabaritoAluno = [$_POST['alternativaQuestao1'], $_POST['alternativaQuestao2'], $_POST['alternativaQuestao3'], $_POST['alternativaQuestao4'], $_POST['alternativaQuestao5']];
-        echo "<h2>GAbarito Oficial: </h2>";
-        for($x = 1; $x <= count($gabaritoOficial); $x++) {
-            echo "<p>Questão $x: ", $gabaritoOficial[$x - 1], "</p>"; 
-        }
+            $gabaritoOficial = ['D', 'D', 'B', 'A', 'A'];
+            $gabaritoAluno = [$_POST['alternativaQuestao1'], $_POST['alternativaQuestao2'], $_POST['alternativaQuestao3'], $_POST['alternativaQuestao4'], $_POST['alternativaQuestao5']];
+            echo "<h2>GAbarito Oficial: </h2>";
+            for($x = 1; $x <= count($gabaritoOficial); $x++) {
+                echo "<p>Questão $x: ", $gabaritoOficial[$x - 1], "</p>"; 
+            }
 
-        echo "<h2> Seu Gabarito: </h2>";
-        for($i = 1; $i <= count($gabaritoAluno); $i++) {
-            echo "<p>Questão $i: ", $gabaritoAluno[$i - 1], "</p>";
-        }
-    ?>
+            echo "<h2> Seu Gabarito: </h2>";
+            for($i = 1; $i <= count($gabaritoAluno); $i++) {
+                echo "<p>Questão $i: ", $gabaritoAluno[$i - 1], "</p>";
+            }
+        ?>
         </div>
 
         <div class="corretasOuIncorretas">
-          <?php 
-                if($gabaritoOficial[0] == $gabaritoAluno[0]) {
-                    echo "<p style='color:green'> Questão 1: Correta </p>";
-                } else {
-                    echo "<p style='color:red'> Questão 1: Incorreta </p>";
-                }
-
-                if($gabaritoOficial[1] == $gabaritoAluno[1]) {
-                    echo "<p style='color:green'> Questão 2: Correta </p>";
-                } else {
-                    echo "<p style='color:red'> Questão 2: Incorreta </p>";
-                }
-
-                if($gabaritoOficial[2] == $gabaritoAluno[2]) {
-                    echo "<p style='color:green'> Questão 3: Correta </p>";
-                } else {
-                    echo "<p style='color:red'> Questão 3: Incorreta </p>";
-                }
-
-                if($gabaritoOficial[3] == $gabaritoAluno[3]) {
-                    echo "<p style='color:green'> Questão 4: Correta </p>";
-                } else {
-                    echo "<p style='color:red'> Questão 4: Incorreta </p>";
-                }
-
-                if($gabaritoOficial[4] == $gabaritoAluno[4]) {
-                    echo "<p style='color:green'> Questão 5: Correta </p>";
-                } else {
-                    echo "<p style='color:red'> Questão 5: Incorreta </p>";
+            <?php
+                for($i = 1; $i <= count($gabaritoOficial); $i++) {
+                    if($gabaritoOficial[$i - 1] == $gabaritoAluno[$i - 1]) {
+                        echo "<p style='color:green'> Questão $i: Correta </p>";
+                    } else {
+                        echo "<p style='color:red'> Questão $i: Incorreta </p>";
+                    }
                 }
             ?>
         </div>
